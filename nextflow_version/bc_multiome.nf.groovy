b9a55cbce48e2510b360f91c8aa82b996b3dcba2
Ryan Mulqueen <mulqueenr@gmail.com>
2025-02-28 04:29:49 -0600

    Update bc_multiome.nf.groovy

--- nextflow_version/bc_multiome.nf.groovy
+++ nextflow_version/bc_multiome.nf.groovy
@@ -313,12 +313,12 @@ workflow {
 			| MERGE_SAMPLES_CALLPEAKS \
 			| set { merged_peaks }
 		}
+/*
 
 	// DATA PREPROCESSING 
 		//Merge filtered seurat objects, add sample metadata
 		cellranger_out = merged_peaks_input | collect
 		merged_seurat_object = MERGE_SAMPLE_AND_FILTER(cellranger_out,sample_metadata)
-/*
 
 		//Merge sample, public data label transfers
 		MERGED_PUBLIC_DATA_LABEL_TRANSFER(merged_seurat_object)
