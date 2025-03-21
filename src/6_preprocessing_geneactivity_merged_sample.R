9f20e852269a9f137e0920484733a52c4979860a
Ryan Mulqueen <mulqueenr@gmail.com>
2025-03-21 15:59:35 -0500

    chromvar update

--- src/6_preprocessing_geneactivity_merged_sample.R
+++ src/6_preprocessing_geneactivity_merged_sample.R
@@ -1,3 +1,6 @@
+#sif="/home/groups/CEDAR/mulqueen/bc_multiome/multiome_bc.sif"
+#singularity shell --bind /home/groups/CEDAR/mulqueen/bc_multiome $sif
+
 library(Signac)
 library(Seurat)
 library(ggplot2)
@@ -6,14 +9,12 @@ library(EnsDb.Hsapiens.v86)
 library(optparse)
 
 option_list = list(
-  make_option(c("-i", "--object_input"), type="character", default=NULL, 
+  make_option(c("-i", "--object_input"), type="character", default="4_merged.chromvar.SeuratObject.rds", 
               help="List of sample RDS files", metavar="character")
 );
 
 opt_parser = OptionParser(option_list=option_list);
 opt = parse_args(opt_parser);
-#setwd("/home/groups/CEDAR/mulqueen/bc_multiome/nf_analysis_round3/seurat_objects")
-#opt$object_input="merged.chromvar.SeuratObject.rds"
 dat=readRDS(opt$object_input)
 
 gene_activity<-GeneActivity(dat,process_n=10000)
@@ -25,6 +26,6 @@ dat<- NormalizeData(
   normalization.method = 'LogNormalize',
   scale.factor = median(dat$nCount_GeneActivity)
 )
-saveRDS(dat,file="4_merged.geneactivity.SeuratObject.rds")
+saveRDS(dat,file="5_merged.geneactivity.SeuratObject.rds")
 
 
