43b31b1c7ffe46c9fd95ab0ab5383c4d4726612c
Ryan Mulqueen <mulqueenr@gmail.com>
2025-05-02 10:45:26 -0500

    update bindings for SIFs

--- nextflow_version/bc_multiome.nf.groovy
+++ nextflow_version/bc_multiome.nf.groovy
@@ -247,7 +247,8 @@ process FIG1_MERGED_CLUSTER {
 }
 
 process FIG2_PSEUDOBULK_ANALYSIS {
-	containerOptions "--bind ${params.proj_dir},${params.src_dir}:/src/,${params.outdir}"
+	//currently uses some local libraries, and so I bound that directory. to be fixed in the SIF
+	containerOptions "--bind ${params.proj_dir},${params.src_dir}:/src/,${params.outdir},/home/rmulqueen/R/x86_64-conda-linux-gnu-library/4.4"
 	publishDir "${params.outdir}/seurat_objects", mode: 'copy', overwrite: true, pattern: "*.rds"
 	publishDir "${params.outdir}/plots/fig2", mode: 'copy', overwrite: true, pattern: "*.pdf"
 	publishDir "${params.outdir}/plots/fig2", mode: 'copy', overwrite: true, pattern: "*.tsv", optional: true
@@ -268,7 +269,8 @@ process FIG2_PSEUDOBULK_ANALYSIS {
 }
 
 process FIG2_SCSUBTYPE {
-	containerOptions "--bind ${params.proj_dir},${params.src_dir}:/src/,${params.outdir},/home/users/mulqueen/R"
+	//currently uses some local libraries, and so I bound that directory. to be fixed in the SIF
+	containerOptions "--bind ${params.proj_dir},${params.src_dir}:/src/,${params.outdir},/home/rmulqueen/R/x86_64-conda-linux-gnu-library/4.4"
 	publishDir "${params.outdir}/seurat_objects", mode: 'copy', overwrite: true, pattern: "*.rds"
 	publishDir "${params.outdir}/plots/fig2", mode: 'copy', overwrite: true, pattern: "*.pdf"
 
